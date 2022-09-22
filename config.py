6847454917987ddcbac91f17f722b7f6db225880
Jose Luis <joseluisgonmar1993@gmail.com>
2022-09-22 21:40:50 +0200

    fix: pipeline

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
@@ -5,3 +7,5 @@ settings = Dynaconf(
     environments=True,
     settings_files=["settings.yaml", ".secrets.yaml"],
 )
+
+TEST_RESOURCES = os.path.join(os.path.dirname(__file__), "tests", "_resources")
